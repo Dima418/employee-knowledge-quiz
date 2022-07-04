f4e50638a186c2de1cccd7d2ebfc7f8d0cdf53a5
Dmytro Dziubenko <dziubenko.dima98@gmail.com>
2022-07-04 21:51:04 +0300

    feature/add_application_factory
    - Add `config.py` with environment variables parser
    - Update `main.py` with `get_application` function

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# from routes import *
+from core import config
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+origins = ["http://localhost:8080", "http://127.0.0.1:8080"]
+
+def get_application():
+    _app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return _app
+
+app = get_application()
+
+# Add routes here
+# app.include_router(router)
