7b381879fb884cf2dd44179c5d1ae666a1c6cfe2
Dmytro Dziubenko <dziubenko.dima98@gmail.com>
2022-07-05 18:35:23 +0300

    feature/add_docker_compose
    - Add `docker-compose.yaml`
    - Add route to test db connection
    - Fix variables in `config.py`

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from databases import Database
+
+from app.core import config
 
 
 router = APIRouter(tags=["home"])
@@ -6,3 +9,12 @@ router = APIRouter(tags=["home"])
 @router.get("/")
 async def home():
     return {"Hello": "World"}
+
+@router.get("/connected")
+async def home():
+    database = Database(config.DATABASE_URL)
+    try:
+        await database.connect()
+        return "Connected successfully"
+    except Exception as e:
+        return "Connection failed", e
